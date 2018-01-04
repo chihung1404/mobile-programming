0f17a2860d60fff33cfb54e492c345bc3acd4e5b
Tran Anh Tuat <trananh200195@gmail.com>
2018-01-05 00:04:45 +0700

    play pause ok

--- thmuzic/screens/playScreen.js
+++ thmuzic/screens/playScreen.js
@@ -34,16 +34,22 @@ const instructions = Platform.select({
     "Double tap R on your keyboard to reload,\n" +
     "Shake or press menu button for dev menu"
 });
-
+var Sound = require("react-native-sound");
 export default class playScreen extends Component<{}> {
   static navigationOptions = ({ navigation }) => ({
     title: `${navigation.state.params.curentSong.title}`
   });
+
+  //--------------------------
+
   constructor(props) {
     super(props);
     var temp = [];
     this.state = {
-      dataSource: temp
+      dataSource: temp,
+      nativeSound: null,
+      currentSong: null,
+      playing: true
     };
     const { params } = this.props.navigation.state;
     params.listSong.forEach(function(item) {
@@ -51,54 +57,97 @@ export default class playScreen extends Component<{}> {
     });
     if (temp[0] != null) {
       this.setState({ ...this.state.dataSource, temp });
+      setTimeout(() => {
+        var whoosh = new Sound(params.curentSong.uri, "", error => {
+          console.log("load Soun");
+          if (error) {
+            console.log("failed to load the sound", error);
+            return;
+          }
+          // loaded successfully
+          this.setState({currentSong: whoosh})
+          console.log('this.state.currentSong',this.state.currentSong)
+          console.log(
+            "duration in seconds: " +
+              whoosh.getDuration() +
+              "number of channels: " +
+              whoosh.getNumberOfChannels()
+          );
+        });
+        setTimeout(() => {
+          this.state.currentSong.play((success) => {
+            if (success) {
+              console.log('successfully finished playing');
+            } else {
+              console.log('playback failed due to audio decoding errors');
+          // reset the player to its uninitialized state (android only)
+          // this is the only option to recover after an error occured and use the player again
+          this.state.currentSong.reset();
+             }
+          });
+        }, 50);
+      }, 250);
     }
     var a = this.state.dataSource.indexOf(params.curentSong);
+    this.startStopButton = this.startStopButton.bind(this);
+    this.handleStartPress = this.handleStartPress.bind(this);
+    this.setPlaying = this.setPlaying.bind(this);
+  }
+
+  //--------------------------
+  setPlaying() {
+    if (this.state.playing) {
+      this.state.currentSong.pause();
+      this.setState({ playing: false });
+      return;
+    } else {
+      this.state.currentSong.play((success) => {
+        if (success) {
+          console.log('successfully finished playing');
+        } else {
+          console.log('playback failed due to audio decoding errors');
+      // reset the player to its uninitialized state (android only)
+      // this is the only option to recover after an error occured and use the player again
+      this.state.currentSong.reset();
+         }
+      });
+      this.setState({ playing: true });
+    }
+  }
+
+  startStopButton() {
+    var img = this.state.playing
+      ? "data:../images/icons/pause.png"
+      : "data:../images/icons/play.png";
+    return (
+      <TouchableOpacity onPress={this.handleStartPress}>
+        <Image
+          style={{ width: 90, height: 90 }}
+          source={
+            this.state.playing
+              ? require("../images/icons/pause.png")
+              : require("../images/icons/play.png")
+          }
+        />
+      </TouchableOpacity>
+    );
+  }
+  handleStartPress() {
+    this.setPlaying();
   }
   render() {
     const { navigate } = this.props.navigation;
     const { params } = this.props.navigation.state;
 
-    var Sound = require("react-native-sound");
-    console.log("new Soun");
+    console.log("new Sound");
     console.log("uri", params.curentSong.uri);
 
-    setTimeout(() => {
-      var whoosh = new Sound(params.curentSong.uri, "", error => {
-        console.log("load Soun");
-        if (error) {
-          console.log("failed to load the sound", error);
-          return;
-        }
-        // loaded successfully
-        console.log(
-          "duration in seconds: " +
-            whoosh.getDuration() +
-            "number of channels: " +
-            whoosh.getNumberOfChannels()
-        );
-      });
-      setTimeout(() => {
-        //whoosh.play((success) => {
-        //  if (success) {
-        //    console.log('successfully finished playing');
-        //  } else {
-        //    console.log('playback failed due to audio decoding errors');
-        // reset the player to its uninitialized state (android only)
-        // this is the only option to recover after an error occured and use the player again
-        //whoosh.reset();
-        //   }
-        //});
-      }, 100);
-    }, 500);
     return (
       <Container style={{ backgroundColor: "rgb(233, 233, 239)" }}>
         <Grid>
           <Row size={4} style={{ backgroundColor: "#635DB7" }}>
             <Col size={1} />
-            <Col
-              size={4}
-              style={styles.rowControl}
-            >
+            <Col size={4} style={styles.rowControl}>
               <Image
                 style={{ width: 350, height: 400 }}
                 source={require("../images/icons/test.jpg")}
@@ -106,10 +155,7 @@ export default class playScreen extends Component<{}> {
             </Col>
             <Col size={1} />
           </Row>
-          <Row
-            size={0.5}
-            style={styles.rowControl}
-          >
+          <Row size={0.5} style={styles.rowControl}>
             <Text>Progress Bar</Text>
           </Row>
 
@@ -131,12 +177,7 @@ export default class playScreen extends Component<{}> {
               </TouchableOpacity>
             </Col>
             <Col size={2} style={styles.rowControl}>
-              <TouchableOpacity onPress={() => navigate("Songs")}>
-                <Image
-                  style={{ width: 90, height: 90 }}
-                  source={require("../images/icons/play.png")}
-                />
-              </TouchableOpacity>
+              {this.startStopButton()}
             </Col>
             <Col size={1} style={styles.rowControl}>
               <TouchableOpacity onPress={() => navigate("Songs")}>
